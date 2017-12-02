20cd120066e1ec83e693e13da3e11a60de4bd08b
annaelde <elde.av@gmail.com>
2017-12-01 23:10:24 -0500

    add unittest.

---
+++ Test.py
@@ -0,0 +1,23 @@
+import unittest
+from EmailParser import Email
+
+class TestEmail(unittest.TestCase):
+    def setUp(self):
+        self.email = Email('')
+
+    def test_open_email(self):
+        """Test if opening a blank path will fail."""
+        with self.assertRaises(Exception):
+            self.email.open_email()
+
+    def test_parse_email(self):
+        """Test if the parsing matches our expected output."""
+        self.email.path = './original_email.txt'
+        self.email.open_email()
+        self.email.parse_email()
+        parsed = open('./parsed_email.txt', 'r')
+        self.assertEqual(parsed.read(), self.email.parsed_email)
+        parsed.close()
+
+if __name__ == '__main__':
+    unittest.main()
