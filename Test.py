26b64a15b2fd8d1135747f1482eab53fc4db5393
annaelde <elde.av@gmail.com>
2017-12-02 00:03:38 -0500

    update attributes and arguments to reflect refactored email class.

--- Test.py
+++ Test.py
@@ -5,7 +5,7 @@ from EmailParser import Email
 
 class TestEmail(unittest.TestCase):
     def setUp(self):
-        self.email = Email('', './parsed_email_test.txt')
+        self.email = Email()
 
     def test_open_email(self):
         """Test if opening a blank path will fail."""
@@ -14,13 +14,15 @@ class TestEmail(unittest.TestCase):
 
     def test_parse_email(self):
         """Test if the parsing matches our expected output."""
-        self.email.path = './original_email.txt'
+        self.email.open_path = './original_email.txt'
         self.email.open_email()
         self.email.parse_email()
         parsed = open('./parsed_email.txt', 'r')
         self.assertMultiLineEqual(parsed.read(), self.email.parsed_email)
         parsed.close()
 
+    def test_save_email(self):
+        pass
 
 if __name__ == '__main__':
     unittest.main()
