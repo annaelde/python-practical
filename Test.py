1425e027be1e834c497f197fc9ef8708119f9352
annaelde <elde.av@gmail.com>
2017-12-02 00:56:30 -0500

    add method docstrings.

--- Test.py
+++ Test.py
@@ -25,6 +25,7 @@ class TestEmail(unittest.TestCase):
         self.assertMultiLineEqual(parsed_email, self.email.parsed_email)
 
     def test_save_email(self):
+        """Test if saving the parsed email to a file works."""
         with self.assertRaises(Exception):
             self.email.save_email('')
 
@@ -32,26 +33,27 @@ class TestEmail(unittest.TestCase):
         os.remove('./saved_email.txt')
 
     def test_mutate_field(self):
+        """Test if adding/removing fields behaves as expected."""
         # Test adding a field
         with self.assertRaises(Exception):
             self.email.add_field('', '')
-        
+
         self.email.add_field(self.key, self.regex)
-        
+
         found_key = False
         found_regex = r''
         for field in self.email.fields:
             if field['key'] == self.key:
                 found_key = True
                 found_regex = field['regex']
-        
+
         self.assertTrue(found_key)
         self.assertEqual(found_regex, self.regex)
 
         # Test removing a field
         with self.assertRaises(Exception):
             self.email.remove_field('')
-        
+
         self.email.remove_field(self.key)
 
         found_key = False
@@ -60,9 +62,10 @@ class TestEmail(unittest.TestCase):
             if field['key'] == self.key:
                 found_key = True
                 found_regex = field['regex']
-        
+
         self.assertFalse(found_key)
         self.assertNotEqual(found_regex, self.regex)
+
 
 if __name__ == '__main__':
     unittest.main()
