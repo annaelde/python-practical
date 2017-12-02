7a696b4bcf91604ceb545152e92846bca520931b
annaelde <elde.av@gmail.com>
2017-12-01 21:51:34 -0500

    add some error handling and extra console options.

--- EmailParser.py
+++ EmailParser.py
@@ -4,7 +4,8 @@ This Python 3 module contains functions for opening a text file and parsing an e
 It can also be run as a script on the command line.
 """
 import re
-
+import sys
+import os
 
 class Email:
     fields = [{'key': 'To',
@@ -28,8 +29,11 @@ class Email:
     def open_email(self):
         """ Read text file and save as string.
         """
-        with open(self.path, 'r') as email_file:
-            self.text = email_file.read()
+        try:
+            with open(self.path, 'r') as email_file:
+                self.text = email_file.read()
+        except:
+            raise Exception
 
     def parse_email(self):
         """ Parse email fields using assigned
@@ -48,7 +52,8 @@ class Email:
         """
         with open(path, 'w') as email_file:
             for field in self.fields:
-                line = '{0}: {1}\n\n'.format(field['key'], '\n'.join(field['value']))
+                line = '{0}: {1}\n\n'.format(
+                    field['key'], '\n'.join(field['value']))
                 email_file.write(line)
 
     def clean_match(self, match):
@@ -60,10 +65,41 @@ class Email:
 
 
 if __name__ == "__main__":
+    def open_email(email):
+        try:
+            email.open_email()
+        except:
+            print('Could not open the specified file.')
+            choice = input('Try a new file (1) or exit (2): ')
+            if choice == '1':
+                email.path = input('Enter the filepath of the email (.txt) to parse: ')
+                email.open_email()
+            else:
+                sys.exit()
+
+        email.parse_email()
+
+    def save_email(email, location):
+        try:
+            email.save_email(location)
+        except:
+            print('Could not save email to that location.')
+            choice = input('Try a new filepath (1) or exit (2): ')
+            if choice == '1':
+                email.path = input('Enter where you want to save the parsed email: ')
+                email.open_email()
+            else:
+                sys.exit()
+    
     # email = Email(input(
     #     'Enter the filepath of the email (.txt) to parse: '))
     # email.save_email(input('Enter where you want to save the parsed email: '))
-    email = Email(r'A:\original_msg.txt')
-    email.open_email()
-    email.parse_email()
-    email.save_email(r'A:\parse_msg.txt')
+
+    parse_my_emails = True
+    while parse_my_emails:
+        email = Email(r'A:')
+        open_email(email)
+        save_email(r'A:\parse_msg.txt')
+        answer = input('Parse another email? (Y / N): ')
+        if answer.lower() != 'y':
+            parse_my_emails = False
